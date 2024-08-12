93aa34615b60b855f59e460c336d5e682cfba08e
Purnama S Rahayu <purnamasrahayu@gmail.com>
2024-08-12 13:40:28 +0700

    check passwoed strength by checkbox

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+// slider
 const characterLengthSlider = document.getElementById("characterLength");
 const characterLengthShowValue = document.querySelector(
   ".form__character-length__value"
 );
+
+// checkboxes
 const checkboxElements = document.querySelectorAll(".checkbox-square");
+const checkboxInputs = document.querySelectorAll(".form__checkbox__input");
+
+// password strength bar
+const passwordStrengthElements = document.querySelectorAll(
+  ".password-generator__strength-bar__container"
+);
+const emptyPasswordElement = document.getElementById("strength--empty");
+const tooWeakPasswordElement = document.getElementById("strength--too-weak");
+const weakPasswordElement = document.getElementById("strength--weak");
+const mediumPasswordElement = document.getElementById("strength--medium");
+const strongPasswordElement = document.getElementById("strength--strong");
 
 function handleSlider(event) {
   const { value } = event.target;
@@ -24,3 +38,33 @@ function handleCheckboxClick(event) {
 checkboxElements.forEach((element) => {
   element.addEventListener("click", handleCheckboxClick);
 });
+
+function checkPasswordStrength() {
+  const checkedCount = Array.from(checkboxInputs).filter(
+    (input) => input.checked
+  ).length;
+
+  passwordStrengthElements.forEach((element) => {
+    element.classList.add("hidden");
+  });
+
+  switch (checkedCount) {
+    case 0:
+      emptyPasswordElement.classList.remove("hidden");
+      break;
+    case 1:
+      tooWeakPasswordElement.classList.remove("hidden");
+      break;
+    case 2:
+      weakPasswordElement.classList.remove("hidden");
+      break;
+    case 3:
+      mediumPasswordElement.classList.remove("hidden");
+      break;
+    case 4:
+      strongPasswordElement.classList.remove("hidden");
+      break;
+    default:
+      break;
+  }
+}
