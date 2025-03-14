f1e3ea790d560b5652f42fdef6c6b2152cd83a56
Purnama S Rahayu <purnamasrahayu@gmail.com>
2025-03-14 07:13:26 +0700

    [FIX] - simplify getRandom function, fix slider inactive style

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // slider
 const characterLengthSlider = document.getElementById("characterLength");
 const characterLengthShowValue = document.querySelector(
-  ".form__character-length__value"
+	".form__character-length__value"
 );
 
 // checkboxes
@@ -10,7 +10,7 @@ const checkboxInputs = document.querySelectorAll(".form__checkbox__input");
 
 // password strength bar
 const passwordStrengthElements = document.querySelectorAll(
-  ".password-generator__strength-bar__container"
+	".password-generator__strength-bar__container"
 );
 const emptyPasswordElement = document.getElementById("strength--empty");
 const tooWeakPasswordElement = document.getElementById("strength--too-weak");
@@ -28,231 +28,204 @@ const iconCopyPassword = document.querySelector(".password-generator__copy");
 const notifyCopyElement = document.querySelector(".password-generator__copied");
 
 const textWarningSlider = document.querySelector(
-  ".text__warning__character-length"
+	".text__warning__character-length"
 );
 
 // character most probable occurences at random
 const characterOccurences = {
-  uppercase: 2,
-  lowercase: 4,
-  number: 2,
-  symbol: 1,
+	uppercase: 2,
+	lowercase: 4,
+	number: 2,
+	symbol: 1,
 };
 
 function handleSlider(event) {
-  const { value } = event.target;
-  characterLengthShowValue.textContent = value;
-  const progress = (value / characterLengthSlider.max) * 100;
-  characterLengthSlider.style.background = `linear-gradient(to right, var(--color-neon-green) ${progress}%, var(--color-black) ${progress}%)`;
-  checkPasswordStrength();
-  textWarningSlider.classList.add("hidden");
+	const { value } = event.target;
+	characterLengthShowValue.textContent = value;
+	const progress = (value / characterLengthSlider.max) * 100;
+	characterLengthSlider.style.background = `linear-gradient(to right, var(--color-neon-green) ${progress}%, var(--color-black) ${progress}%)`;
+	checkPasswordStrength();
+	textWarningSlider.classList.add("hidden");
+	generateButton.disabled = false;
 }
 
 characterLengthSlider.addEventListener("input", handleSlider);
 
 function handleCheckboxClick(event) {
-  const checkboxId = event.target.id;
-  const inputId = checkboxId.split("-")[1];
+	const checkboxId = event.target.id;
+	const inputId = checkboxId.split("-")[1];
 
-  const inputCheckElement = document.getElementById(inputId);
-  inputCheckElement.checked = !inputCheckElement.checked;
-  checkPasswordStrength();
-  textWarningSlider.classList.add("hidden");
+	const inputCheckElement = document.getElementById(inputId);
+	inputCheckElement.checked = !inputCheckElement.checked;
+	checkPasswordStrength();
+	textWarningSlider.classList.add("hidden");
+	generateButton.disabled = false;
 }
 
 checkboxElements.forEach((element) => {
-  element.addEventListener("click", handleCheckboxClick);
+	element.addEventListener("click", handleCheckboxClick);
 });
 
 checkboxInputs.forEach((element) => {
-  element.addEventListener("change", function () {
-    checkPasswordStrength();
-    textWarningSlider.classList.add("hidden");
-  });
+	element.addEventListener("change", function () {
+		checkPasswordStrength();
+		textWarningSlider.classList.add("hidden");
+		generateButton.disabled = false;
+	});
 });
 
 function checkPasswordStrength() {
-  let strength = "";
-  const checkedCount = Array.from(checkboxInputs).filter(
-    (input) => input.checked
-  ).length;
-  const characterLength = characterLengthSlider.value;
-  if (characterLength == 0) {
-    strength = "empty";
-  } else if (characterLength < 6 || checkedCount <= 1) {
-    strength = "too weak";
-  } else if (
-    (characterLength >= 6 && characterLength <= 8) ||
-    checkedCount === 2
-  ) {
-    strength = "weak";
-  } else if (characterLength >= 13 && checkedCount === 4) {
-    strength = "strong";
-  } else if (characterLength >= 9 && checkedCount >= 3) {
-    strength = "medium";
-  }
-
-  passwordStrengthElements.forEach((element) => {
-    element.classList.add("hidden");
-  });
-
-  switch (strength) {
-    case "empty":
-      emptyPasswordElement.classList.remove("hidden");
-      break;
-    case "too weak":
-      tooWeakPasswordElement.classList.remove("hidden");
-      break;
-    case "weak":
-      weakPasswordElement.classList.remove("hidden");
-      break;
-    case "medium":
-      mediumPasswordElement.classList.remove("hidden");
-      break;
-    case "strong":
-      strongPasswordElement.classList.remove("hidden");
-      break;
-    default:
-      break;
-  }
+	passwordStrengthElements.forEach((element) => {
+		element.classList.add("hidden");
+	});
+	const checkedCount = Array.from(checkboxInputs).filter(
+		(input) => input.checked
+	).length;
+	const characterLength = characterLengthSlider.value;
+	if (characterLength == 0) {
+		emptyPasswordElement.classList.remove("hidden");
+	} else if (characterLength < 6 || checkedCount <= 1) {
+		tooWeakPasswordElement.classList.remove("hidden");
+	} else if (
+		(characterLength >= 6 && characterLength <= 8) ||
+		checkedCount === 2
+	) {
+		weakPasswordElement.classList.remove("hidden");
+	} else if (characterLength >= 13 && checkedCount === 4) {
+		strongPasswordElement.classList.remove("hidden");
+	} else if (characterLength >= 9 && checkedCount >= 3) {
+		mediumPasswordElement.classList.remove("hidden");
+	}
 }
 
 generateButton.addEventListener("click", function (e) {
-  e.preventDefault();
+	e.preventDefault();
 
-  const charLength = parseInt(characterLengthSlider.value);
-  const checkedCriterias = Array.from(checkboxInputs).filter(
-    (element) => element.checked
-  );
-  const checkedCriteriaIds = checkedCriterias.map((item) => item.id);
-  const password = generatePassword(charLength, checkedCriteriaIds);
-  if (password) {
-    passwordElement.textContent = password;
-    passwordElement.style.opacity = 1;
-    notifyCopyElement.classList.add("hidden");
-  }
+	const charLength = parseInt(characterLengthSlider.value);
+	const checkedCriterias = Array.from(checkboxInputs).filter(
+		(element) => element.checked
+	);
+	const checkedCriteriaIds = checkedCriterias.map((item) => item.id);
+	const password = generatePassword(charLength, checkedCriteriaIds);
+	if (password) {
+		passwordElement.textContent = password;
+		passwordElement.style.opacity = 1;
+		notifyCopyElement.classList.add("hidden");
+	}
 });
 
 function generateRandomChar(value) {
-  let character = "";
-  switch (value) {
-    case "uppercase":
-      character = getRandomUppercaseChar();
-      break;
-    case "lowercase":
-      character = getRandomLowercaseChar();
-      break;
-    case "number":
-      character = getRandomNumber();
-      break;
-    case "symbol":
-      character = getRandomSymbol();
-      break;
-    default:
-      break;
-  }
+	let character = "";
+	switch (value) {
+		case "uppercase":
+			character = getRandom(65, 90); // Random uppercase letter (A-Z)
+			break;
+		case "lowercase":
+			character = getRandom(97, 122); // Random lowercase letter (a-z)
+			break;
+		case "number":
+			character = getRandom(48, 57); // Random number (0-9) as char
+			break;
+		case "symbol":
+			character = getRandomSymbol();
+			break;
+		default:
+			break;
+	}
 
-  return character;
+	return character;
 }
 
 function generatePassword(length, criterias) {
-  if (length < criterias.length) {
-    textWarningSlider.classList.remove("hidden");
-    return;
-  }
+	if (length < criterias.length) {
+		textWarningSlider.classList.remove("hidden");
+		generateButton.disabled = true;
+		return;
+	}
 
-  let passwordString = "";
+	let passwordString = "";
 
-  //   generate string to fulfill checked criterias
-  for (let i = 0; i < criterias.length; i++) {
-    let criteria = criterias[i];
-    let randomCharacter = generateRandomChar(criteria);
-    passwordString += randomCharacter;
-  }
+	//   generate string to fulfill checked criterias
+	for (let i = 0; i < criterias.length; i++) {
+		let criteria = criterias[i];
+		let randomCharacter = generateRandomChar(criteria);
+		passwordString += randomCharacter;
+	}
 
-  //   generate rest of the password
-  const weights = criterias.map((criteria) => characterOccurences[criteria]);
-  const remainingLength = length - criterias.length;
-  for (let i = 0; i < remainingLength; i++) {
-    let value = getRandomWeightedValue(criterias, weights);
-    let randomCharacter = generateRandomChar(value);
-    passwordString += randomCharacter;
-  }
+	//   generate rest of the password
+	const weights = criterias.map((criteria) => characterOccurences[criteria]);
+	const remainingLength = length - criterias.length;
+	for (let i = 0; i < remainingLength; i++) {
+		let value = getRandomWeightedValue(criterias, weights);
+		let randomCharacter = generateRandomChar(value);
+		passwordString += randomCharacter;
+	}
 
-  const shuffledPassword = shuffleString(passwordString);
-  return shuffledPassword;
+	const shuffledPassword = shuffleString(passwordString);
+	return shuffledPassword;
 }
 
 function shuffleString(string) {
-  // Copy the array to avoid modifying the original
-  const arrayOfChars = string.split("");
-  const shuffledArray = arrayOfChars.slice();
+	// Copy the array to avoid modifying the original
+	const arrayOfChars = string.split("");
+	const shuffledArray = arrayOfChars.slice();
 
-  // Fisher-Yates shuffle algorithm
-  for (let i = shuffledArray.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    // Swap elements at indices i and j
-    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
-  }
+	// Fisher-Yates shuffle algorithm
+	for (let i = shuffledArray.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		// Swap elements at indices i and j
+		[shuffledArray[i], shuffledArray[j]] = [
+			shuffledArray[j],
+			shuffledArray[i],
+		];
+	}
 
-  const shuffledString = shuffledArray.join("");
-  return shuffledString;
+	const shuffledString = shuffledArray.join("");
+	return shuffledString;
 }
 
-function getRandomUppercaseChar() {
-  // Generate a random number between 65 and 90 (ASCII values for A-Z)
-  const randomCode = Math.floor(Math.random() * 26) + 65;
-  // Convert the ASCII code to a character
-  return String.fromCharCode(randomCode);
-}
-
-function getRandomLowercaseChar() {
-  // Generate a random number between 97 and 122 (ASCII values for a-z)
-  const randomCode = Math.floor(Math.random() * 26) + 97;
-  // Convert the ASCII code to a character
-  return String.fromCharCode(randomCode);
-}
-
-function getRandomNumber() {
-  return Math.floor(Math.random() * 10);
+function getRandom(min, max) {
+	return String.fromCharCode(
+		Math.floor(Math.random() * (max - min + 1)) + min
+	);
 }
 
 function getRandomSymbol() {
-  const symbols = "!@#$%^&*()_+-=[]{}|;:',.<>?/`~";
-  const randomIndex = Math.floor(Math.random() * symbols.length);
-  return symbols[randomIndex];
+	const symbols = "!@#$%^&*()_+-=[]{}|;:',.<>?/`~";
+	const randomIndex = Math.floor(Math.random() * symbols.length);
+	return symbols[randomIndex];
 }
 
 function getRandomWeightedValue(values, weights) {
-  // Step 1: Create cumulative weights
-  const cumulativeWeights = weights.reduce((acc, weight) => {
-    const lastValue = acc.length > 0 ? acc[acc.length - 1] : 0;
-    acc.push(lastValue + weight);
-    return acc;
-  }, []);
+	// Step 1: Create cumulative weights
+	const cumulativeWeights = weights.reduce((acc, weight) => {
+		const lastValue = acc.length > 0 ? acc[acc.length - 1] : 0;
+		acc.push(lastValue + weight);
+		return acc;
+	}, []);
 
-  // Step 2: Generate a random number between 0 and the total weight
-  const random =
-    Math.random() * cumulativeWeights[cumulativeWeights.length - 1];
+	// Step 2: Generate a random number between 0 and the total weight
+	const random =
+		Math.random() * cumulativeWeights[cumulativeWeights.length - 1];
 
-  // Step 3: Find which range the random number falls into
-  const index = cumulativeWeights.findIndex((weight) => random < weight);
+	// Step 3: Find which range the random number falls into
+	const index = cumulativeWeights.findIndex((weight) => random < weight);
 
-  // Return the value at the found index
-  return values[index];
+	// Return the value at the found index
+	return values[index];
 }
 
 function copyPasswordToClipboard() {
-  const password = passwordElement.textContent;
-  if (password !== passwordPlaceholder)
-    navigator.clipboard
-      .writeText(password)
-      .then(() => {
-        notifyCopyElement.classList.remove("hidden");
-      })
-      .catch((error) => {
-        console.error("failed to copy password", error);
-      });
+	const password = passwordElement.textContent;
+	if (password !== passwordPlaceholder)
+		navigator.clipboard
+			.writeText(password)
+			.then(() => {
+				notifyCopyElement.classList.remove("hidden");
+			})
+			.catch((error) => {
+				console.error("failed to copy password", error);
+			});
 }
 
 iconCopyPassword.addEventListener("click", copyPasswordToClipboard);
